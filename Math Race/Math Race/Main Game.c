48f7b5e2deb2dffe134c7f7df515ea07bdc686cf
GoombaFangs <GoombaFangs>
2025-04-08 03:11:34 +0300

    start the game

--- Math Race/Math Race/Main Game.c
+++ Math Race/Math Race/Main Game.c
@@ -2,24 +2,34 @@
 
 #define MAX_PLAYERS 3
 
-
-
-int main() {
-    printf("Welcome to Math Race!\n\n");
-    
-
+void InputPlayerName() 
+{
     struct Player players[MAX_PLAYERS];
     printf("Enter Your name: ");
     fgets(players[0].name, sizeof(players[0].name), stdin);
+}
 
-    // Remove newline character if present
-    size_t len = strlen(players[0].name);
-    if (len > 0 && players[0].name[len - 1] == '\n') {
-        players[0].name[len - 1] = '\0';
-    }
-   
+void main() 
+{
+    printf("Welcome to Math Race!\n\n");
+	InputPlayerName();
+	printf("game tuturial\n");
 
-    printf("\n--- Player List ---\n");
-    printf("Player name: %s\n", players[0].name);
+	int round = 0;
+	int try = 0;
+	int penalty = 0;
+	
+
+	//timer start
+
+	for (int i = 0; i < 10; i++)
+	{
+		//random question by difficukty (bass on round number)
+		printf("Question %d: What is %d + %d?\n", i + 1, i, i);	// need to get random number
+		int answer;
+		scanf_s("%d", &answer);
+		printf("Your answer: %d\n", answer);
+		//answer check
+	}
 
 }
