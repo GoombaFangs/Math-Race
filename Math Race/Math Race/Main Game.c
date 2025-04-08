33578ed5def6ba963cb49ec59672883b3f8f7f48
GoombaFangs <GoombaFangs>
2025-04-08 14:52:13 +0300

    add End Game Sceen

--- Math Race/Math Race/Main Game.c
+++ Math Race/Math Race/Main Game.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h> 
-#include <stddef.h>
 #include "Player.c"
 
 
@@ -9,46 +8,50 @@
 #endif
 
 
-void NewPlayer() 
-{
+void NewPlayer(int playerNumber)
+{	
     printf("Enter Your name: ");
 	char name[50];
 	scanf_s("%49s", name, (unsigned int)_countof(name));
-	strcpy_s(player[0].name, sizeof(player[0].name), name);
-	name[_countof(name) - 1] = '\0';
-	printf("Your Name is: %s\n", player[0].name);
+	strcpy_s(player[playerNumber].name, sizeof(name), name);
 
 }
 
-//void EndGameSceen()
-//{
-//	printf("Thanks for playing!\n\n\nprass...\nA)Try again\nB)New Player\nC)Exit...\n");
-//	char choice[10];
-//	scanf_s("%9s", choice, (unsigned)_countof(choice));
-//	if (strcmp(choice, "a") == 0 || strcmp(choice, "Try again") == 0 || strcmp(choice, "Tryagain") == 0)// Try Agian
-//	{
-//		printf("Try again\n");
-//	}
-//	else if (strcmp(choice, "b") == 0 || strcmp(choice, "New Player") == 0 || strcmp(choice, "NewPlayer") == 0)// New Player
-//	{
-//		printf("New Player\n");
-//	}
-//	else if (strcmp(choice, "c") == 0 || strcmp(choice, "Exit") == 0)// Exit
-//	{
-//		printf("Exit\n");
-//		exit(0);
-//	}
-//	else
-//	{
-//		printf("Invalid choice..\n");
-//		EndGameSceen();
-//	}
-//
-//}
+void EndGameSceen()
+{
+	printf("Thanks for playing!\n\n\nprass...\nA)Try again\nB)New Player\nC)Exit...\n");
+	char choice[10];
+	scanf_s("%9s", choice, (unsigned)_countof(choice));
+	if (strcmp(choice, "a") == 0 || strcmp(choice, "Try again") == 0 || strcmp(choice, "Tryagain") == 0)// Try Agian
+	{
+		printf("Try again\n");
+	}
+	else if (strcmp(choice, "b") == 0 || strcmp(choice, "New Player") == 0 || strcmp(choice, "NewPlayer") == 0)// New Player
+	{
+		printf("New Player\n");
+	}
+	else if (strcmp(choice, "c") == 0 || strcmp(choice, "Exit") == 0)// Exit
+	{
+		printf("Exit\n");
+		exit(0);
+	}
+	else
+	{
+		printf("Invalid choice..\n");
+		EndGameSceen();
+	}
+
+}
 void main() 
 {
     printf("Welcome to Math Race!\n\n");
-	NewPlayer();
+	int playerCount = 0;
+	NewPlayer(playerCount);
+	playerCount++;
+
+	printf("Player 1 name is: %s\n", player[0].name);
+
+
 	printf("game tuturial\n");// need to be more detailed
 
 	int round = 0;
@@ -60,7 +63,7 @@ void main()
 	{
 		//timer start
 		printf("Round %d\n", round + 1);
-		for (int i = 0; i < 10; i++)//round details
+		for (int i = 0; i < 2; i++)//round details
 		{
 			//random question by difficukty (bass on round number)
 			printf("Question %d: What is %d + %d?\n", i + 1, i, i);	// need to get random number
@@ -71,5 +74,5 @@ void main()
 		//timer stop
 		round++;
 	}
-
+	EndGameSceen();
 }
--- Math Race/Math Race/Player.c
+++ Math Race/Math Race/Player.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-
 #define MAX_PLAYERS 3
 
 typedef struct player {
